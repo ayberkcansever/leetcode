9d9d164f6dc6fcf2c163692f6cfd6eac20eee00b
Ayberk Cansever <ayberk.cansever@gmail.com>
2022-06-02 15:57:40 +0300

    NumberOfIslands solution added

---
+++ src/main/java/com/canseverayberk/leetcode/medium/NumberOfIslands.java
@@ -0,0 +1,49 @@
+package com.canseverayberk.leetcode.medium;
+
+public class NumberOfIslands {
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '1'},
+                {'0', '0', '0', '1', '0'}
+        };
+        int numIslands = numIslands(grid);
+    }
+
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    sink(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void sink(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1')
+            return;
+
+        grid[i][j] = '0';
+
+        sink(grid, i - 1, j);
+        sink(grid, i + 1, j);
+        sink(grid, i, j - 1);
+        sink(grid, i, j + 1);
+    }
+}
