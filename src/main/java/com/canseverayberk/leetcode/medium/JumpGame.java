54da1d9cc0d32116684fd714bdf49d2be8228934
ayberk.cansever <ayberk.cansever@trendyol.com>
2022-06-23 23:10:12 +0300

    JumpGame solution added

---
+++ src/main/java/com/canseverayberk/leetcode/medium/JumpGame.java
@@ -0,0 +1,27 @@
+package com.canseverayberk.leetcode.medium;
+
+/*
+https://leetcode.com/problems/jump-game
+ */
+public class JumpGame {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2,3,1,1,4};
+        boolean canJump = canJump(nums);
+    }
+
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 1)
+            return true;
+
+        int maxIndex = nums[0];
+        if (maxIndex == 0)
+            return false;
+        for(int i = 1; i < nums.length - 1; i++) {
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+            if (maxIndex <= i)
+                return false;
+        }
+        return (maxIndex >= nums.length - 1);
+    }
+}
