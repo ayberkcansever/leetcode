e032dba2342c75e35ccfae250656cbbe90c0ac54
Ayberk Cansever <ayberk.cansever@trendyol.com>
2022-07-07 18:42:55 +0300

    GroupAnagrams solution added

---
+++ src/main/java/com/canseverayberk/leetcode/medium/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package com.canseverayberk.leetcode.medium;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> groupAnagrams = groupAnagrams(strs);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+
+        for (List<String> list : map.values()) {
+            result.add(list);
+        }
+        return result;
+    }
+}
