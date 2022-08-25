89afa917179e41b04347f0a7dff5770b745da84e
Ayberk Cansever <ayberkcansever@gmail.com>
2022-08-25 16:45:06 +0300

    MinimumDepthOfBinaryTree solution added

---
+++ src/main/java/com/canseverayberk/leetcode/easy/bfs/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,67 @@
+package com.canseverayberk.leetcode.easy.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+https://leetcode.com/problems/minimum-depth-of-binary-tree
+ */
+public class MinimumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node15 = new TreeNode(15);
+        TreeNode node20 = new TreeNode(20);
+
+        node3.left = node9;
+        node3.right = node20;
+        node20.left = node15;
+        node20.right = node7;
+
+        int minDepth = minDepth(node3);
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int minDepth = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return minDepth;
+                }
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            minDepth++;
+        }
+
+        return minDepth;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
