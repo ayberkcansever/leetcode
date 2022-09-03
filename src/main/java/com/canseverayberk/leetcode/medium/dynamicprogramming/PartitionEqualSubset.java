b69cfb9f33b3daf43d2f383cd26cf1c4998819a9
Ayberk Cansever <ayberkcansever@gmail.com>
2022-09-03 18:37:28 +0300

    PartitionEqualSubset solution converted to bottom-up approach

--- src/main/java/com/canseverayberk/leetcode/medium/dynamicprogramming/PartitionEqualSubset.java
+++ src/main/java/com/canseverayberk/leetcode/medium/dynamicprogramming/PartitionEqualSubset.java
@@ -12,30 +12,47 @@ public class PartitionEqualSubset {
 
     public static boolean canPartition(int[] nums) {
         int totalSum = 0;
-        for (int i : nums) {
+        for(int i : nums) {
             totalSum += i;
         }
         if (totalSum % 2 == 1)
             return false;
 
-        int sum = totalSum / 2;
-        Boolean[][] dp = new Boolean[nums.length + 1][sum + 1];
-        return search(nums, 0, sum, dp);
-    }
+        int searched = totalSum / 2;
 
-    public static boolean search(int[] nums, int i, int sum, Boolean[][] dp) {
-        if (sum == 0)
-            return true;
-        if (sum < 0)
-            return false;
-        if (i >= nums.length)
-            return false;
+        boolean[][] dp = new boolean[nums.length][searched + 1];
 
-        Boolean result = dp[i][sum];
-        if (result == null) {
-            result = search(nums, i + 1, sum - nums[i], dp) || search(nums, i + 1, sum, dp);
-            dp[i][sum] = result;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 1; j < searched + 1; j++) {
+                boolean prev = false;
+                if(i >= 1) {
+                    prev = dp[i - 1][j];
+                }
+
+                boolean diff = false;
+                int difference = j - nums[i];
+                if (i >= 1 && difference >= 0) {
+                    diff = dp[i-1][difference];
+                }
+
+                boolean me = (j - nums[i]) == 0;
+                dp[i][j] = prev || diff || me;
+            }
         }
-        return result;
+
+        return dp[nums.length - 1][searched];
     }
+
+/*
+
+        [9,1,2,4,10]  -> 13
+
+        i/j     1   2   3   4   5   6   7   8   9   10  11  12  13
+        9       F   F   F   F   F   F   F   F   T   F   F   F   F
+        1       T   F   F   F   F   F   F   F   T   T   F   F   F
+        2       T   T   T   F   F   F   F   F   T   T   T   T   F
+        4       T   T   T   T   T   T   T   F   T   T   T   T   F
+        10      T   T   T   T   T   T   T   T   T   T   T   T   T
+
+ */
 }
